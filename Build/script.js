7f150b0ca50214505ea84aed76279e37f22d28f3
Tunegee23 <ogunsanya.opeyem23@gmail.com>
2024-07-29 01:50:03 -0700

    portfolio

--- Build/script.js
+++ Build/script.js
@@ -1,3 +1,4 @@
+
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     if (section) {
@@ -7,3 +8,26 @@ function scrollToSection(sectionId) {
         });
     }
 }
+
+    document.addEventListener("DOMContentLoaded", function(){
+        const menuToggle = document.getElementById("menu-toggle")
+        const menu = document.getElementById("menu")
+    
+        menuToggle.addEventListener("click", function(){
+            menu.classList.toggle("active");
+            if(menu.classList.contains("active")) {
+                menu.style.display="flex";
+            } else {
+                menu.style.display="none"
+            }
+        })
+    });
+
+   
+        const menuLinks = document.querySelectorAll(".menu a");
+        menuLinks.forEach(link => {
+            link.addEventListener("click", function() {
+                menu.classList.remove("active");
+            });
+        });
+    
